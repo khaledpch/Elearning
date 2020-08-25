6a554822455eae11515a4102dd78983088bf4c5a
Khaled tlili <deanpch@gmail.com>
2020-08-25 14:40:37 +0100

    change bd conx

--- admin/conx.php
+++ admin/conx.php
@@ -1,4 +1,14 @@
 <?php
-mysql_connect("localhost","root","") or die("No Connection");
-mysql_select_db("isims") or die("No Database name");
+$username="root";  
+$password="";  
+$hostname = "localhost";  
+//connection string with database  
+$dbhandle = mysqli_connect($hostname, $username, $password)  
+or die("Unable to connect to MySQL");  
+echo "";  
+// connect with database  
+$selected = mysqli_select_db($dbhandle, "isims")  
+or die("Could not select examples");  
+//mysqli_connect("localhost","root","") or die("No Connection");
+//mysql_select_db("isims") or die("No Database name");
 ?>
